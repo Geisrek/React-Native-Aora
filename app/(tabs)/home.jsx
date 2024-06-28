3ff509866fd0a4a7eb81ef97e6d66c8057c8a22b
Rafik Hijazy <rafikhz90@gmail.com>
2024-06-28 03:12:58 +0300

    Create SearchInput component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView,Image } from 'react-native'
 import React from 'react'
 import { useNavigation } from 'expo-router'
+import { FlatList, GestureHandlerRootView } from 'react-native-gesture-handler'
+import { images } from '../../constants'
+import SearchInput from '../../components/SearchInput'
 const Home = () => {
    
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <GestureHandlerRootView>
+    <SafeAreaView className='bg-primary'>
+        <FlatList
+        data={[{id:0},{id:0}]}
+        keyExtractor={(item)=>item.$id}
+        renderItem={({item})=>(
+            <Text className='text-3xl text-white'>{item.id}</Text>
+        )}
+        ListHeaderComponent={()=>(
+        <View className='my-6 px-4 space-y-6'>
+                <View className='justify-between items-start flex-row mb-6'>
+                    <View>
+                     <Text className='font-pmedium text-sm text-gray-100'>Welcome back</Text>
+                     <Text className='text-2xl font-psemibold text-white '>Comrade</Text>
+                     </View>
+                     <View className='mt-1.5'>
+                        <Image
+                        source={images.logoSmall}
+                        className='w-9 h-10'
+                        resizeMode='contain'
+                        />
+                     </View>
+                </View>
+                <SearchInput/>
+            </View>)
+        }
+        />
+    </SafeAreaView>
+    </GestureHandlerRootView>
   )
 }
 
---
+++ components/SearchInput.jsx
@@ -0,0 +1,34 @@
+import { View, Text, TouchableOpacity ,Image} from 'react-native'
+import React, { useState } from 'react'
+import { TextInput ,GestureHandlerRootView } from 'react-native-gesture-handler'
+import {icons} from '../constants'
+const SearchInput = ({title,value,keyboardType,placeholder,handleChangeText,otherStyles,...props}) => {
+    const [showPassword,setShowPassword]=useState(false)
+  return (
+    <GestureHandlerRootView>
+   
+      <View className="border-2  w-full h-16 px-4 bg-black-200
+      rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+       
+        <TextInput
+        className='text-base mt-0.5 text-white flex-1 font-pregular'
+        value={value}
+        placeholder={placeholder}
+        onChangeText={handleChangeText}
+        placeholderTextColor='#7b7b8b'
+        secureTextEntry={title=='Password'&&!showPassword}
+        />
+      <TouchableOpacity>
+        <Image
+        source={icons.search}
+        className='x-5 h-5'
+        resizeMode='contain'
+        />
+      </TouchableOpacity>
+      </View>
+   
+    </GestureHandlerRootView>
+  )
+}
+
+export default SearchInput
