1c3f35bb7a83f853a5fa0e1233fc86329dae49dc
Rafik Hijazy <rafikhz90@gmail.com>
2024-07-08 02:38:27 +0300

    Implement the Welvome component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@
 import { Link, SplashScreen, Stack } from 'expo-router';
 import { useFonts } from 'expo-font';
 import { useEffect } from 'react';
+import GlobalProvider from '../context/GlobalProvider';
 SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [fontsLoaded,error]=useFonts({
@@ -31,11 +32,13 @@ export default function RootLayout() {
     return null;
   }
   return(
+   <GlobalProvider>
     <Stack>
       <Stack.Screen name='index' options={{headerShown:false, }}/>
       <Stack.Screen name='(auth)' options={{headerShown:false, }}/>
       <Stack.Screen name='(tabs)' options={{headerShown:false, }}/>
     </Stack>
+    </GlobalProvider>
   )
 }
 //<Stack.Screen name='/search/[query]' options={{headerShown:false, }}/>
