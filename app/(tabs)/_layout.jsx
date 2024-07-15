e632d40f251cf8ed04cf40f8966dc01de826b591
Rafik Hijazy <rafikhz90@gmail.com>
2024-07-15 04:42:25 +0300

    Implement the create component

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -53,22 +53,7 @@ const TabsLayouts = () => {
             ),
           }}
         />
-        <Tabs.Screen
-        name='bookmark'
-        options={{
-          tabBarLabel:'',
-          title:'Bookmark',
-          headerShown:false,
-          tabBarIcon:({color,focused})=>(
-           <TabIcon
-           name='bookmark'
-           icon={icons.bookmark}
-           color={color}
-           focused={focused}/>)
-
-          
-        }
-        }/>
+       
           <Tabs.Screen
         name='create'
         options={{
